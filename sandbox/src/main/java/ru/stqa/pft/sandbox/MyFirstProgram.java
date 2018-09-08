2f440326af834196f5755af25958387dccc58e5e
mmaljutin <mmaljutin@gmail.com>
2018-09-08 23:07:51 +0300

    Code reformated

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,25 +6,20 @@ public class MyFirstProgram {
     hello("world");
     hello("user");
     hello("Max");
+
     Square s = new Square(5);
     System.out.println("Plodhadj kvadrata so storonoj " + s.l + " = " + s.area());
-Rectangle r = new Rectangle(4,6);
+
+    Rectangle r = new Rectangle(4, 6);
     System.out.println("Ploshadj treugolnika so storonami " + r.a + " i " + r.b + " = " + r.area());
 
-    Point p1 = new Point(2,4);
-    Point p2 = new Point(1,3);
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(1, 3);
     double dist = Point.distance(p1, p2);
     System.out.println("Koordinatq tochek na dvumernoj ploskosti:" + " x1 = " + p1.x + " y1 = " + p1.y + "; x2 = " + p2.x + " y2 = " + p2.y + " | " + "Rasstojanie mezhdu to4kami = " + dist);
-
   }
-
 
   public static void hello(String somebody) {
-
     System.out.println("Hello, " + somebody + "!");
   }
-
-
-
-
 }
