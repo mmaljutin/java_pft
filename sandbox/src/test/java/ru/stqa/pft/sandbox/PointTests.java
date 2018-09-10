ba1d3c521832750725c82c2d0f6abe431eae6cb5
mmaljutin <25511582+mmaljutin@users.noreply.github.com>
2018-09-10 10:53:42 +0300

    PointTests.java updated

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,8 +8,7 @@ public class PointTests {
   public void testArea() {
     Point p1 = new Point(2, 4);
     Point p2 = new Point(1, 3);
-    double px = p2.x - p1.x;
-    double py = p2.y - p1.y;
-    Assert.assertEquals(Math.sqrt(px * px + py * py), 1.4142135623730951);
+    double dist = Point.distance(p1, p2);
+    Assert.assertEquals(dist, 1.4142135623730951);
   }
 }
