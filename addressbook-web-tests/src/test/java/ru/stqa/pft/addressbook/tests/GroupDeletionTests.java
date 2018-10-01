d8fec9371f9d8dfed6b368cf48676f181ec8c1ad
mmaljutin <mmaljutin@gmail.com>
2018-10-01 21:24:19 +0300

    finding maximum in collection

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -12,7 +12,7 @@ public class GroupDeletionTests extends TestBase {
 
 
     @Test
-    public void testGroupDeletion() throws Exception {
+    public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
@@ -23,6 +23,10 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() -1);
+            Assert.assertEquals(before, after);
+
     }
 
 }
