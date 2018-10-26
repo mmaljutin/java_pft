75956c7b6a9e61c20d5f9cf5a6d200986845de31
mmaljutin <mmaljutin@gmail.com>
2018-10-27 00:24:24 +0300

    Revert "DbHelper added"
    This reverts commit bea3c77a8d816afdd44fb10f2f02f62a422cb1c4.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -27,7 +27,6 @@ public class ApplicationManager {
 
     private StringBuffer verificationErrors = new StringBuffer();
     private String browser;
-    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -37,9 +36,6 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        dbHelper = new DbHelper();
-
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -80,9 +76,5 @@ public class ApplicationManager {
 
     public ContactHelper contact() {
         return contactHelper;
-    }
-
-    public DbHelper db() {
-        return dbHelper;
     }
 }
